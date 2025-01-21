494db93c35810b938347c0bdfadc9637d778e1c0
Levi Wilkerson <levi.wilkerson@proton.me>
2025-01-21 00:05:14 -0400

    assign functions to variables, slice range access, append std func

--- arrays/sum.go
+++ arrays/sum.go
@@ -7,3 +7,24 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(array ...[]int) []int {
+	var sums []int
+	for _, numbers := range array {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(array ...[]int) []int {
+	var sums []int
+	for _, numbers := range array {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("Adds numbers from a collection of any size.", func(t *testing.T) {
@@ -13,4 +16,34 @@ func TestSum(t *testing.T) {
 		}
 	})
 
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 1, 1}, []int{2, 2, 2})
+	want := []int{3, 6}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("Got '%v', want '%v'", got, want)
+	}
+
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t *testing.T, got, want []int) {
+		if !slices.Equal(got, want) {
+			t.Errorf("Got '%v', want '%v'", got, want)
+		}
+	}
+
+	t.Run("Returns sum of tails", func(t *testing.T) {
+		got := SumAllTails([]int{1, 6}, []int{4, 10, 10}, []int{2})
+		want := []int{6, 20, 0}
+		checkSums(t, got, want)
+	})
+
+	t.Run("Safely sums empty array", func(t *testing.T) {
+		got := SumAllTails([]int{})
+		want := []int{0}
+		checkSums(t, got, want)
+	})
 }
